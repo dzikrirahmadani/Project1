caa72a4ca4ab0bf95bda8d8757391be6d22d4a34
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-08-31 14:54:37 +0700

    update animasi title pada halaman data-supplier

--- dist/dashboardScript/dataSupplier.js
+++ dist/dashboardScript/dataSupplier.js
@@ -1,9 +1,16 @@
 const content = document.querySelector('#cont-1');
 const content2 = document.querySelector('#cont-2');
+
+const navbarTable = document.querySelectorAll('.navbar-table');
 const onload = () => {
     document.body.style.opacity = '1';
     document.body.style.transition = 'all .6s .2s ease';
 
     content.style.animation = 'myanimate 5s .6s forwards';
     content2.style.animation = 'myanimate 5s .6s forwards';
+
+    navbarTable.forEach(el => {
+        el.style.opacity = '1';
+        el.style.transition = 'all .6s .6s ease-in';
+    })
 }
