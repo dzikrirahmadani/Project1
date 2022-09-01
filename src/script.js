c90acf4c80e206141b4f116b04ba4954f3e94610
unknown <dzikrirahmadani211@gmail.com>
2022-09-01 14:20:18 +0700

    responsive semua form

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+const navInput = document.querySelector('.nav-input');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
