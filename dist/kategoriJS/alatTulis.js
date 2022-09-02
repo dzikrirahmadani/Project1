18fc2ca6cbef2f0b7ca3be709950064e651e6f67
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-02 09:31:44 +0700

    add animasi ketika di load pada halaman alatTulis

--- dist/kategoriJS/alatTulis.js
+++ dist/kategoriJS/alatTulis.js
@@ -4,29 +4,14 @@ const onload = () => {
   
     // event dashboard barang masuk onload
     const conTable = document.querySelectorAll('#content');
-    const titleTable = document.querySelectorAll('#title');
+    const titleTable = document.querySelectorAll('.title-table');
   
     conTable.forEach(el => {
       el.style.animation = 'myanimate 5s .6s forwards';
     })
-    
+
     titleTable.forEach(el => {
-      if( window.innerWidth > 1023 && window.innerWidth <= 1218){
-          styles(el, '20%');
-      }else if( window.innerWidth > 1218 ){
-          styles(el, '17%');
-      }else if( window.innerWidth < 1024){
-          styles(el, '5%');
-      }
-      if( window.innerWidth <= 447 ){
-         styles(el, '-2rem');
-      }
+        el.style.opacity = '1';
+        el.style.transition = 'all .6s .6s ease-in-out';
     })
-  }
-  
-  const styles = (titleTable, marginLeftTitle) => {
-    titleTable.style.opacity = '1';
-    titleTable.style.marginLeft = marginLeftTitle;
-    titleTable.style.width = '80%';
-    titleTable.style.transition = 'all .6s .6s ease-in';
   }
